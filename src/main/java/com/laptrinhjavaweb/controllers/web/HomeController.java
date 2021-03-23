fbafc91b79d2ac0bc43d7023d9d1fb5bb9ef548c
Mạnh Vũ Văn <vuvanmanhttvtvp@gmail.com>
2021-03-24 01:37:57 +0700

    setup project

---
+++ src/main/java/com/laptrinhjavaweb/controllers/web/HomeController.java
@@ -0,0 +1,33 @@
+package com.laptrinhjavaweb.controllers.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.laptrinhjavaweb.models.User;
+
+@WebServlet(urlPatterns = {"/home-page"})
+public class HomeController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user = new User();
+		user.setFullName("Vũ Văn Mạnh");
+		
+		req.setAttribute("model", user);
+		RequestDispatcher rd = req.getRequestDispatcher("/views/web/home.jsp");
+		rd.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+}
